d4ca1b8e9837497012fd74c982df6d874ac49cc8
Tyson151 <troexc77@gmail.com>
2020-12-31 15:10:19 -0700

    Initial Commit

---
+++ Udder2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder2
+//
+//  Created by Tyson O'Steen on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
